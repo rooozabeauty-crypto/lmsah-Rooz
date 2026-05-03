b32d50a26a2a9f506db30eeaa1a75406e27aa527
رموز احمد <rooozabeauty@gmail.com>
2026-05-03 04:09:44 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+// استدعاء الراوتر
+const webhookRoutes = require("./callback");
+app.use("/", webhookRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
